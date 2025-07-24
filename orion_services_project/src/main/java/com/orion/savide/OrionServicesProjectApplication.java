4ab72472f9da883abf03d938c2088b772904e9ca
devinalzate <devinalzate2005@gmail.com>
2025-07-23 22:21:04 -0500

    inicializacion de aplicacion
    Con las primeras dependencias requeridas

---
+++ orion_services_project/src/main/java/com/orion/savide/OrionServicesProjectApplication.java
@@ -0,0 +1,13 @@
+package com.orion.savide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrionServicesProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrionServicesProjectApplication.class, args);
+    }
+
+}
---
+++ orion_services_project/src/test/java/com/orion/savide/OrionServicesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orion.savide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrionServicesProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
